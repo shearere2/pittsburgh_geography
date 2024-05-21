e25fb46373da7251728ecfdea77c71c9124e5278
shearere2 <shearere2@duq.edu>
2024-05-21 16:24:19 -0400

    Edits to read in DEM

--- pittsburgh_geography/reader/read_and_write.py
+++ pittsburgh_geography/reader/read_and_write.py
@@ -1,13 +1,23 @@
 from osgeo import gdal
 import numpy as np
 from matplotlib import pyplot as plt
+import rasterio as rio
 
-ds = gdal.Open('data/Allegheny_County_1m/PAMAP_DEM_mosaic_Allegheny_1m.tif')
-geotransform = ds.GetGeoTransform()
-proj = ds.GetProjection()
+# ds = gdal.Open('data/Allegheny_County_1m/PAMAP_DEM_mosaic_Allegheny_1m.tif')
+# geotransform = ds.GetGeoTransform()
+# proj = ds.GetProjection()
 
-band = ds.GetRasterBand(ds.RasterCount)
-array = band.ReadAsArray()
+# band = ds.GetRasterBand(ds.RasterCount)
+# array = band.ReadAsArray()
 
-plt.figure()
-plt.imshow(array)
+# plt.figure()
+# plt.imshow(array)
+# plt.savefig('data/pittsburgh')
+
+dem = rio.open('data/Allegheny_County_1m/PAMAP_DEM_mosaic_Allegheny_1m.tif')
+dem_array = dem.read(1).astype('float64')
+
+fig, ax = plt.subplots(1, figsize=(12,12))
+rio.plot.show(dem_array, cmap='Greys_r',ax=ax)
+plt.axis('off')
+plt.savefig('data/pittsburgh')
